12444c485bc7050a3bf2abbf86ed2bccbb0ba828
jonasneuf <jonasneuf@gmail.com>
2023-11-07 13:09:53 -0600

    first commit

---
+++ src/pages/PageAbout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Jwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo optio
+        illum cumque blanditiis hic molestiae delectus totam placeat ipsam atque
+        harum dolore laborum, asperiores voluptatem aut provident quos tenetur
+        perferendis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo optio
+        illum cumque blanditiis hic molestiae delectus totam placeat ipsam atque
+        harum dolore laborum, asperiores voluptatem aut provident quos tenetur
+        perferendis.
+      </p>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageAbout",
+});
+</script>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-page>
+    <div class="q-py-lg q-px-md">
+      <q-input
+        bottom-slots
+        v-model="newJweetContent"
+        placeholder="What's happening"
+        counter
+        autogrow
+        class="new-jweet"
+        maxlength="280"
+        :dense="dense"
+      >
+        <template v-slot:before>
+          <q-avatar size="xl">
+            <img src="src/assets/onepunch.jpg" style="object-fit: cover" />
+          </q-avatar>
+        </template>
+
+        <template v-slot:after>
+          <q-btn
+            :disable="!newJweetContent"
+            unelevated
+            no-caps
+            rounded
+            color="primary"
+            label="jweet"
+          />
+        </template>
+      </q-input>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageHome",
+  data() {
+    return {
+      newJweetContent: "",
+    };
+  },
+});
+</script>
+
+<style lang="sass">
+.new-jweet
+  textarea
+    font-size: 19px
+    linehight: 1.4 !important
+</style>
